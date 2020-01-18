f675e7d33689b195d64d356a55a85cb744af3357
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2020-01-18 02:20:18 +0100

    Ajoutez des fichiers projet.

---
+++ AdScoreShow/Controllers/FileProcessingController.cs
@@ -0,0 +1,38 @@
+﻿using AdScoreShow.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdScoreShow.Controllers
+{
+    public class FileProcessingController : Controller
+    {
+        // GET: FileProcessing
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult NewFileProcessing()
+        {
+            var uploadedFile = new AdvertisementViewModel();
+
+            return View(uploadedFile);
+        }
+
+        [HttpPost]
+        public ActionResult NewFileProcessing(AdvertisementViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ AdScoreShow/Models/AdvertisementViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AdScoreShow.Models
+{
+    public class AdvertisementViewModel
+    {
+        public Advertisement Advertisement { get; set; }
+
+        [Required(ErrorMessage = "A CSV file is required.")]
+        [DataType(DataType.Upload)]
+        [Display(Name = "Upload a CSV File here...")]
+        public HttpPostedFileBase UpLoadedCsvFile { get; set; }
+    }
+}
