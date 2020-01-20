6be8a62df9a2a355a4e4f080a0886aee56d17286
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2020-01-20 04:25:04 +0100

    Adding DataShowController and AdvertViewModel

--- AdScoreShow/Controllers/DataShowController.cs
+++ AdScoreShow/Controllers/DataShowController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdScoreShow.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,10 @@ namespace AdScoreShow.Controllers
         // GET: DataShow
         public ActionResult Index()
         {
+            using(var dbContext = new AdScoreShowDbContext())
+            {
+
+            }
             return View();
         }
     }
---
+++ AdScoreShow/Models/AdvertViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdScoreShow.Models
+{
+    public class AdvertViewModel
+    {
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public string Brand { get; set; }
+        public string Segment { get; set; }
+        public int Year { get; set; }
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+    }
+}
